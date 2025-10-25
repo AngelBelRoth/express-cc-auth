88a6df23c1ae0785f91e1ab67f0da171004602d2
ANGÉL <asa@ANGEL.local>
2025-10-25 18:01:17 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+module.exports = {
+
+    // 'url' : 'mongodb+srv://demo:demo@cluster0.q2ojb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
+    'url' : 'mongodb+srv://angelbel:ZrnVrinjZp7vcjUy@cluster0.nuqkq6x.mongodb.net/demo?appName=Cluster0', 
+    'dbName': 'demo'
+};
